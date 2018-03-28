e29c57f3f2780eb2ade8b3b0d37a17d1fca2b65a
javierferwolf <jbenitezbasantes@gmail.com>
2018-03-28 09:50:29 +0300

    Update SM.ml

--- src/SM.ml
+++ src/SM.ml
@@ -23,14 +23,40 @@ type config = int list * Syntax.Stmt.config
 
    Takes a configuration and a program, and returns a configuration as a result
  *)                         
-let eval _ = failwith "Not yet implemented"
+let rec eval (stack, (s, i, o)) p =
+ 	  if (List.length p)=0 then (stack, (s, i, o))
+ 	  else eval
+ 	  (match (List.hd p) with
+ 	    BINOP op -> (match stack with first::second::rest ->
+ 		     ((Syntax.Expr.eval s (Syntax.Expr.Binop op (Syntax.Expr.Const second) (Syntax.Expr.Const first)))::rest, (s, i, o)))
+ 	    |CONST x -> (x::stack, (s, i, o))
+ 	    |READ -> ((List.hd i)::stack, (s, List.tl i, o))
+ 	    |WRITE -> (match stack with first::rest -> (rest, (s, i, o @ [first])))
+ 	    |LD x -> ((s x)::stack, (s, i, o))
+ 	    |ST x -> (match stack with first::rest -> (rest, (Syntax.Expr.update x first s, i, o)))
+ 	   ) (List.tl p);;
+
+(* Top-level evaluation
+     val run : int list -> prg -> int list
+   Takes an input stream, a program, and returns an output stream this program calculates
+*)
+let run i p = let (_, (_, _, o)) = eval ([], (Syntax.Expr.empty, i, [])) p in o
 
 (* Stack machine compiler
-
      val compile : Syntax.Stmt.t -> prg
-
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
 
-let compile _ = failwith "Not yet implemented"
+ let rec compile stmt =
+ 	let rec compile_exp exp =
+match exp with
+         	Syntax.Expr.Const c -> [CONST c]
+          |Syntax.Expr.Var v -> [LD v]
+        	|Syntax.Expr.Binop(op, l, r) -> (compile_exp l) @ (compile_exp r) @ [BINOP op]
+ 	in
+ 	match stmt with
+ 	  Syntax.Stmt.Read x -> [READ; ST x]
+ 	  |Syntax.Stmt.Write exp -> (compile_exp exp) @ [WRITE]
+ 	  |Syntax.Stmt.Assign(x, exp) -> (compile_exp exp) @ [ST x]
+         |Syntax.Stmt.Seq(l, r) -> (compile l) @ (compile r) ;;
